5105c042e011d6af2b36c8bd219723d0bcbbfca0
mihirs2310 <mihir.savkar2003@gmail.com>
2024-10-24 18:31:21 -0400

    committed

--- ResumeRescue/settings.py
+++ ResumeRescue/settings.py
@@ -12,19 +12,22 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-from decouple import config
-
+from dotenv import load_dotenv  # Import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Load environment variables from the .env file
+load_dotenv(os.path.join(BASE_DIR, '.env'))  # Specify the correct path to your .env file
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-tr7n=m0i1lt1r17sq3x75q1mmbkf5ov9rv*)1ofik)-t8jw4$l"
+SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-tr7n=m0i1lt1r17sq3x75q1mmbkf5ov9rv*)1ofik)-t8jw4$l")
 
-OPENAI_API_KEY = config('OPEN_AI_TOKEN')
+# Use the API key from the environment variable
+OPENAI_API_KEY = os.getenv("OPEN_AI_TOKEN")  # Access the OpenAI API key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -32,7 +35,6 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 # Application definition
-
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
@@ -73,10 +75,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "ResumeRescue.wsgi.application"
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
@@ -84,10 +84,8 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
@@ -97,21 +95,15 @@ AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
-
 LANGUAGE_CODE = "en-us"
-
 TIME_ZONE = "UTC"
-
 USE_I18N = True
-
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_URL = '/static/'
 
 # Media files (Uploaded files)
